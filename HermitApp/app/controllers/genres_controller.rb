0728e9b4c1062d66d91f02b22767a59ee343331a
Samantha Rabling <sjarillorabling@gmail.com>
2018-06-30 15:53:44 -0500

    working on controllers

--- HermitApp/app/controllers/genres_controller.rb
+++ HermitApp/app/controllers/genres_controller.rb
@@ -1,5 +1,5 @@
 class GenresController < ApplicationController
-    before_action :require_user, only: [:show, :update]
+    # before_action :require_user, only: [:show, :update]
 
     def index
         @genres = Genre.all
@@ -8,14 +8,10 @@ class GenresController < ApplicationController
 
     def create 
         @genres = Genre.create!(genre_params)
-        json_response(@genres, :created)
+        json_response(@genres, :create)
     end
 
-    def show 
-        json_response(@genres)
-    end
-
-    def update
+    def clicked
         @user = current_user
         @genres = Genre.find(params[:id])
         @status = Status.create(user_id: @user.id, genre_id:@genres.id)
@@ -27,7 +23,7 @@ class GenresController < ApplicationController
         params.permit(:name, :description)
     end
 
-    def require_user
-        @user = User.find(params[:id])
-    end
+    # def require_user
+    #     @user = User.find(params[:id])
+    # end
 end
--- HermitApp/config/routes.rb
+++ HermitApp/config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users do
-    resources :genres
-    resources :sessions
-  end  
+  root 'users#index'
+
+  post 'users/create' => 'users#create'
+
+  post 'sessions/create' => 'sessions#create'
+
+  get 'genres' => 'genres#index'
+
+  post 'genres/create' => 'genres#create'
+
+  patch 'genres/:id/clicked' => 'ideas#clicked'
+
+  get 'logout' => 'sessions#delete'
 end
